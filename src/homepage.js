191f862197e25c251850299045eaaed32efee6a4
M Ulmer <mju9999@gmail.com>
2025-02-17 21:24:02 -0800

    Add homepage js module with basic layout content

---
+++ src/homepage.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const header = document.createElement("h1");
+header.textContent = "Second Breakfast Food Truck";
+
+const bodyDiv = document.createElement("div");
+bodyDiv.className = "body-div";
+
+const bodyText = document.createElement("p");
+bodyText.textContent = "Sometimes a bowl of dry cereal in the car doesn't keep you. Have a proper breakfast: listen for the jingle as our truck passes by mid-morning.";
+
+const imagePlaceholder = document.createElement("div");
+imagePlaceholder.className = "image";
+
+const contentContainer = document.querySelector("#content");
+contentContainer.appendChild(header);
+contentContainer.appendChild(bodyDiv);
+bodyDiv.appendChild(bodyText);
+bodyDiv.appendChild(imagePlaceholder);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 "use strict";
 
-console.log("Running index.js.");
+import "./homepage.js";
